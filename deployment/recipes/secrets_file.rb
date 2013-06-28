baa39c36ba6acb64e818a930bf7de9193f8b0bf7
Chris Radcliff <chris.radcliff@gmail.com>
2013-06-28 11:57:34 -0700

    Specify variables.

--- deployment/recipes/secrets_file.rb
+++ deployment/recipes/secrets_file.rb
@@ -4,15 +4,15 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
   template "#{deploy[:deploy_to]}/shared/config/too_many_secrets.rb" do
-    source "too_many_secrets.rb"
+    source "too_many_secrets.rb.erb"
     cookbook 'deployment'
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
     
-    variables(:database => deploy[:database], :environment => deploy[:rails_env])
+    variables(:secret => deploy[:secrets])
 
-    # notifies :run, resources(:execute => "restart Rails app #{application}")
+    notifies :run, resources(:execute => "restart Rails app #{application}")
 
     only_if do
       File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/shared/config/")
